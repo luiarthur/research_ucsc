1d2f476f8d6cf450983005191582bbe2322e3673
Arthur Lui <luiarthur@gmail.com>
2016-05-01 18:59:15 -0700

    yes

--- graphics/choro2.jl
+++ graphics/choro2.jl
@@ -1,6 +1,5 @@
-using ChoroplethMaps, DataFrames, Plots, Gadfly
-default(size=(1500, 750))
-
+using ChoroplethMaps, DataFrames, Gadfly
+set_default_plot_size(1500px, 750px)
 
 df_theft = readtable("ca_theft.csv",header=true)
 mf0 = mapify(df_theft, Provider.COUNTYSUMMARY(), key=:NAME, keepcols=[:STATEFP])
@@ -9,9 +8,7 @@ mf = mf0[mf0[:STATEFP] .== "06",:]
 # Plotting the outline of some counties
 layer1 = Gadfly.layer(mf, x=:CM_X, y=:CM_Y, group=:NAME, Theme(default_color=colorant"black"),
                       Geom.polygon(preserve_order=true, fill=false)) # preserve_order ensures the points are traced in order. 
-
 Gadfly.plot(layer1, Coord.cartesian(fixed=true)) # Coord.cartesian: Prevents the map from being stretched out.
-Gadfly.plot(layer1,layer2)
 
 # Adding a second layer
 center_x = -1.33e7
@@ -19,7 +16,7 @@ center_y = 4.5e6
 sd = 2e5
 n=100
 layer2 = layer(x=randn(n)*sd+center_x, y=randn(n)*sd+center_y, color=randn(n), Geom.point, Theme(default_color=colorant"orange"))
-layer2 = layer(x=randn(n)*sd+center_x, y=randn(n)*sd+center_y, color=randn(n), Geom.rectbin)
-plot(layer1,layer2, Coord.cartesian(fixed=true))
+layer2 = layer(x=randn(n)*sd+center_x, y=randn(n)*sd+center_y, color=randn(n), Geom.point, Theme(colorkey_swatch_shape=:square))
+Gadfly.plot(layer1,layer2, Coord.cartesian(fixed=true))
 
 
