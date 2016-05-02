44ea114abdcd67a2c2342040dfa4ee978561c4cc
Arthur Lui <luiarthur@gmail.com>
2016-05-01 21:58:09 -0700

    yes

--- graphics/testplotlyjs.jl
+++ graphics/testplotlyjs.jl
@@ -2,6 +2,20 @@ println("loading PlotlyJS")
 using PlotlyJS, Rsvg
 
 t1 = scatter(;x=[1, 2, 3, 4, 5], y=[1, 6, 3, 6, 1])
-p1 = plot(t1)
+p = plot(t1)
 
-PlotlyJS.savefig2(p1, "output_filename.pdf")
+#=
+  include("testplotlyjs.jl")
+  PlotlyJS.savefig3(p, "p.pdf")
+=#
+
+#x = randn(100)
+#y = randn(100)
+#z = randn(100)
+#
+#t2 = plot(scatter(;x=x, y=y,mode="lines+markers",marker_size=3))
+#plot(scatter(;x=x, y=y,mode="markers",marker=attr(size=10,symbol=:square,color=:red)))
+#
+#plot([Plot(t1) Plot(t1)])
+
+
